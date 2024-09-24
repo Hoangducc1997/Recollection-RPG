f9733a49cd35512a877cd8b365768f42935a7a9c
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-09-24 18:57:09 +0700

    new

---
+++ Assets/TenGame/Scripts/NPC/NPCConst.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NPCType
+{
+    Boss,
+    Creep,
+    NPC_Mission,
+    NPC
+}
+
+public enum NPCName
+{
+
+}
