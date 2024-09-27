ceb3788d7cdd94b654f435e8b53cb22f81864c73
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-09-28 00:39:41 +0700

    new

--- Assets/Script/Manager/SceneManager/SampleSceneManager.cs
+++ Assets/Script/Manager/SceneManager/SampleSceneManager.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCManager : MonoBehaviour
+public class SampleSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        UIManager.Instance.ShowPopup(PopupName.SamplePopup);
     }
 
     // Update is called once per frame
