56f4017ab09e3c7380c515677f80a9c8fad334ed
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-10-11 00:36:09 +0700

    new

--- Assets/Script/Map/Entrance.cs
+++ Assets/Script/Map/Entrance.cs
@@ -9,12 +9,12 @@ using UnityEngine.Rendering.Universal;
 public class Entrance : MonoBehaviour
 {
     private Animator animator;
-    [SerializeField] private Light2D globalLight; // Reference to the Global Light 2D
+    [SerializeField] private Animator lightAnimator;
     [SerializeField] private CinemachineVirtualCamera virtualCamera; // Reference to the Cinemachine virtual camera
     public float targetOrthoSize = 5f; // The desired orthographic size
     public float smoothSpeed = 2f; // Speed of the transition
-    public Color targetLightColor = Color.red; // The desired color for the Global Light 2D
     [SerializeField] private GameObject rainEffect; // Reference to the GameObject to activate
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -24,12 +24,14 @@ public class Entrance : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {
-            ChangeGlobalLightColor(targetLightColor);
+        {       
             // Activate the Rain Effect
             ActivateObject();
+
             // First, change the camera orthographic size
             StartCoroutine(ChangeCameraOrthoSize(targetOrthoSize));
+
+            lightAnimator.SetTrigger("ActivetDarkLight"); // Trigger the light animation
         }
     }
 
@@ -48,16 +50,9 @@ public class Entrance : MonoBehaviour
 
         virtualCamera.m_Lens.OrthographicSize = targetSize; // Ensure the exact target size is reached
 
-        // Once the camera size change is done, trigger the door animation
         animator.SetBool("isOpen", true);
     }
-    private void ChangeGlobalLightColor(Color newColor)
-    {
-        if (globalLight != null)
-        {
-            globalLight.color = newColor; // Change the color of the global light
-        }
-    }
+ 
     private void ActivateObject()
     {
         if (rainEffect != null)
