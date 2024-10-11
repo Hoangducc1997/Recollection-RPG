a3e34775230fb77a62c81805dc5c199949237a57
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-10-11 18:40:05 +0700

    New

--- Assets/Resources/Joystick Pack/Scripts/Joysticks/VariableJoystick.cs
+++ Assets/Resources/Joystick Pack/Scripts/Joysticks/VariableJoystick.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+
 public class VariableJoystick : Joystick
 {
     public float MoveThreshold { get { return moveThreshold; } set { moveThreshold = Mathf.Abs(value); } }
---
+++ Assets/TutorialManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField] private GameObject touchSkipTutorial;
+
+    private void Start()
+    {
+        touchSkipTutorial.SetActive(true); // Enable the object when the game starts
+    }
+
+    private void Update()
+    {
+        DetectTouch(); // Check for touch input every frame
+    }
+
+    private void DetectTouch()
+    {
+        // Check if the player touches the screen on mobile or clicks the left mouse button on PC
+        if (Input.GetMouseButtonDown(0) || Input.touchCount > 0)
+        {
+            touchSkipTutorial.SetActive(false); // Disable the touchSkipTutorial object
+        }
+    }
+}
