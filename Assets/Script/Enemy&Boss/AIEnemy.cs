1e4c4a4389af359ed38899b25aa5f76e392474cf
Hoangducc1997 <Hoangducc1997@gmail.com>
2024-10-28 22:11:52 +0700

    Add/ Fea/ A*Pathfinding

--- Assets/Script/Enemy&Boss/AIEnemy.cs
+++ Assets/Script/Enemy&Boss/AIEnemy.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
@@ -6,18 +6,43 @@ using Pathfinding;
 public class AIEnemy : MonoBehaviour
 {
     private AIPath path;
+    private Animator animator;
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform target;
-    // Start is called before the first frame update
+
+    private bool isFacingRight = true;
+
     private void Start()
     {
         path = GetComponent<AIPath>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         path.maxSpeed = moveSpeed;
         path.destination = target.position;
+
+        // Di chuyển animation khi enemy đang di chuyển
+        animator.SetBool("isRun", path.velocity.magnitude > 0);
+
+      
+        // Quay mặt về phía player
+        if (target.position.x > transform.position.x && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (target.position.x < transform.position.x && isFacingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
--- Assets/Script/Enemy&Boss/EnemyHeath.cs
+++ Assets/Script/Enemy&Boss/EnemyHeath.cs
@@ -1,27 +1,51 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
 {
+    private Animator animator;
     [SerializeField] private int maxHealth = 100; // Lượng máu tối đa của Enemy
     private int currentHealth;
+    [SerializeField] private float timeForAnimDeath = 1f; // Thời gian chờ cho animation isDeath
 
     void Start()
     {
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth; // Khởi tạo máu ban đầu bằng máu tối đa
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage; // Trừ máu
-        if (currentHealth <= 0)
+
+        if (currentHealth > 0)
+        {
+            animator.SetBool("isHurt", true); // Gọi anim "isHurt" khi bị mất máu
+            StartCoroutine(ResetHurtAnimation()); // Reset isHurt sau khi anim kết thúc
+        }
+        else if (currentHealth <= 0)
         {
             Die(); // Gọi hàm Die nếu máu bằng 0 hoặc ít hơn
         }
     }
 
+    private IEnumerator ResetHurtAnimation()
+    {
+        // Đợi cho animation "isHurt" hoàn thành
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        animator.SetBool("isHurt", false); // Reset lại trạng thái isHurt
+    }
+
     void Die()
     {
-        // Bạn có thể thêm hiệu ứng biến mất ở đây, như hiệu ứng particle
+        animator.SetBool("isDeath", true); // Gọi hiệu ứng "isDeath"
+        StartCoroutine(WaitForDeathAnimation());
+    }
+
+    private IEnumerator WaitForDeathAnimation()
+    {
+        // Chờ một khoảng thời gian cố định trước khi xóa đối tượng
+        yield return new WaitForSeconds(timeForAnimDeath);
         Destroy(gameObject); // Xóa Enemy khỏi game
     }
 }
